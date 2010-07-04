79976484c56dee0cb23b45f3ca5934fd4a5bd294
Sebastian Deutsch <sebastian.deutsch@9elements.com>
2010-07-04 02:40:31 +0200

    Removed rack endpoint by using application octet for encoding thx @colinschlueter

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Html5fileupload::Application.routes.draw do |map|
 #  match "/upload/:id" => proc {|env| [200, {}, ["Hello world"]] }
-  match "/upload/:id" => UploadEndpoint  
-#  match "/upload/:id" => 'frontend#upload'
+#  match "/upload/:id" => UploadEndpoint  
+  match "/upload/:id" => 'frontend#upload'
   root :to => 'frontend#index'
 end
